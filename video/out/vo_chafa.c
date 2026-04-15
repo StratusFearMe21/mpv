64c6d0eed6dead9210dbd042b80738bb4255aca7
Isaac Mills <rooster0055@protonmail.com>
2026-04-15 14:26:31 -0600

    vo_chafa: use the TA_FREEP macro where applicable

--- video/out/vo_chafa.c
+++ video/out/vo_chafa.c
@@ -208,8 +208,7 @@ static void dealloc_canvas_and_buffers(struct vo *vo)
     }
 
     if (priv->frame) {
-        talloc_free(priv->frame);
-        priv->frame = NULL;
+        TA_FREEP(&priv->frame);
     }
 }
 
@@ -597,7 +596,7 @@ static bool draw_frame(struct vo *vo, struct vo_frame *frame)
                                    priv->frame->stride[0]);
 
     if (mpi)
-        talloc_free(mpi);
+        TA_FREEP(&mpi);
 
 done:
     return VO_TRUE;
